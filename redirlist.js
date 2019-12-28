2a47f6e5e69a6dd3da3db672a4f4385b7ce9196b
Mlntcandy <31708294+mlntcandy@users.noreply.github.com>
2019-12-28 20:58:39 +0300

    Create redirlist.js

---
+++ redirlist.js
@@ -0,0 +1,8 @@
+const redirlist = {
+
+example_google: 'https://google.com/',
+example_yandex: 'https://yandex.ru/',
+example_youtube: 'https://youtube.com/',
+example_reddit: 'https://reddit.com/'
+
+}
