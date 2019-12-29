c89f5fefdb903fe5779c18d40b8c276a87cacb06
Mlntcandy <31708294+mlntcandy@users.noreply.github.com>
2019-12-29 11:12:01 +0300

    Update redirlist.js

--- redirlist.js
+++ redirlist.js
@@ -3,6 +3,8 @@ const redirlist = {
 example_google: 'https://google.com/',
 example_yandex: 'https://yandex.ru/',
 example_youtube: 'https://youtube.com/',
-example_reddit: 'https://reddit.com/'
+example_reddit: 'https://reddit.com/',
+
+kaivachat: 'https://vk.me/join/AJQ1d295EAUmenkGuaCpN/iN'
 
 }
